1d20d601bb78a268f00442e113acf185e6655256
Yousuf Hasan <yousuf85@gmail.com>
2020-04-14 06:08:41 +1000

    extract prisma secret to env variables

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -3,5 +3,5 @@ import jwt from 'jsonwebtoken';
 export const generateToken = (userId) => {
    return jwt.sign({
         userId: userId
-    }, 'thisisasecret', {'expiresIn': '7 days'});
+    }, process.env.AUTH_SECRET, {'expiresIn': '7 days'});
 }
